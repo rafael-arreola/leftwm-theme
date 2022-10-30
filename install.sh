30e3b725b78d634dff81ce5d7c7cf3de37c793f6
Carlos Rafael Arreola Hernández <rafael-arreola@outlook.com>
2022-10-29 21:41:39 -0600

    prepare keybinding

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ -x "$(command -v paru)"]; then
-  paru -Syu --noconfirm polybar feh picom dunst leftwm leftwm-theme-git
+  paru -Syu --noconfirm rofi alacritty polybar feh picom dunst leftwm leftwm-theme-git leftwm-config-git
 fi
 
 if [ -x "$(command -v yay)"]; then
-  yay -Syu --noconfirm polybar feh picom dunst leftwm leftwm-theme-git
+  yay -Syu --noconfirm rofi alacritty polybar feh picom dunst leftwm leftwm-theme-git leftwm-config-git
 fi
 
 git clone https://github.com/rafael-arreola/leftwm-theme.git ~/.config/leftwm/themes/develop

f7f5ff4a6b75c43691d80b808076deb1c62fc4c9
Carlos Rafael Arreola Hernández <rafael-arreola@outlook.com>
2022-12-16 22:36:29 -0700

    Polybar fixed

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ -x "$(command -v paru)" ]; then
-  paru -Syu --noconfirm rofi alacritty polybar feh picom dunst leftwm leftwm-theme-git leftwm-config-git ttf-fira-code ttf-cascadia-code ttf-iosevka-nerd ttc-iosevka ttf-font-awesome
+  paru -Syu --noconfirm flameshot rofi alacritty polybar feh picom dunst leftwm leftwm-theme-git leftwm-config-git ttf-fira-code ttf-cascadia-code ttf-iosevka-nerd ttc-iosevka ttf-font-awesome
 fi
 
 if [ -x "$(command -v yay)" ]; then
-  yay -Syu --noconfirm rofi alacritty polybar feh picom dunst leftwm leftwm-theme-git leftwm-config-git ttf-fira-code ttf-cascadia-code ttf-iosevka-nerd ttc-iosevka ttf-font-awesome
+  yay -Syu --noconfirm flameshot rofi alacritty polybar feh picom dunst leftwm leftwm-theme-git leftwm-config-git ttf-fira-code ttf-cascadia-code ttf-iosevka-nerd ttc-iosevka ttf-font-awesome
 fi
 
 git clone https://github.com/rafael-arreola/leftwm-theme.git ~/.config/leftwm/themes/develop
